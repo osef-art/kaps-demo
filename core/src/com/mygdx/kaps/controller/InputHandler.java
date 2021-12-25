32f202391a46682a1f9d4849ac7abacc72e8d67a
adam <82466060+osef-art@users.noreply.github.com>
2021-12-25 22:52:34 +0100

    Implement holdable keys

--- core/src/com/mygdx/kaps/controller/InputHandler.java
+++ core/src/com/mygdx/kaps/controller/InputHandler.java
@@ -2,28 +2,39 @@ package com.mygdx.kaps.controller;
 
 import com.badlogic.gdx.InputProcessor;
 import com.mygdx.kaps.level.Level;
+import com.mygdx.kaps.time.Timer;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class InputHandler implements InputProcessor {
     public enum Key {
-        LEFT_KEY(21, 45), // Q, LEFT ARR.
-        RIGHT_KEY(22, 32), // D, RIGHT ARR.
-        FLIP_KEY(19, 54), // Z, UP ARR.
-        DOWN_KEY(20, 47), // S, DOWN ARR.
-        DROP_KEY(62), // C, L
-        HOLD_KEY(31, 50, 30), // V, M
+        LEFT_KEY(Level::moveGeluleLeft, true, 21, 45), // Q, LEFT ARR.
+        RIGHT_KEY(Level::moveGeluleRight, true, 22, 32), // D, RIGHT ARR.
+        DOWN_KEY(Level::dipOrAcceptGelule, true, 20, 47), // S, DOWN ARR.
+        FLIP_KEY(Level::flipGelule, true, 19, 54), // Z, UP ARR.
+        DROP_KEY(Level::dropGelule, 62), // C, L
+        HOLD_KEY(Level::holdGelule, 31, 50, 30), // V, M
 
-        ESCAPE_KEY(29, 131, 68), // A, ESC, !
+        ESCAPE_KEY(m -> System.exit(0), 29, 131, 68), // A, ESC, !
         ;
 
         private final Set<Integer> codes;
+        private final Consumer<Level> effect;
+        private final boolean holdable;
 
-        Key(int... codes) {
+        Key(Consumer<Level> consumer, int... codes) {
+            this(consumer, false, codes);
+        }
+
+        Key(Consumer<Level> consumer, boolean hold, int... codes) {
             this.codes = Arrays.stream(codes).boxed().collect(Collectors.toUnmodifiableSet());
+            effect = consumer;
+            holdable = hold;
         }
 
         public static Optional<Key> ofCode(int code) {
@@ -34,12 +45,25 @@ public class InputHandler implements InputProcessor {
         public String toString() {
             return "[" + super.toString().split("_")[0] + "]";
         }
+
+        public void applyOn(Level level) {
+            effect.accept(level);
+        }
+
+        public boolean canBeHold() {
+            return holdable;
+        }
     }
 
+    private final HashMap<Key, Timer> pressedKeys = new HashMap<>();
     private final Level model;
 
     public InputHandler(Level model) {
         this.model = model;
+    }
+
+    public void update() {
+        pressedKeys.values().forEach(Timer::resetIfExceeds);
     }
 
     // input detection
@@ -48,34 +72,16 @@ public class InputHandler implements InputProcessor {
         // debug
 //        System.out.println(keycode);
         Key.ofCode(keycode).ifPresent(key -> {
-            switch (key) {
-                case ESCAPE_KEY:
-                    System.exit(0);
-                case LEFT_KEY:
-                    model.moveGeluleLeft();
-                    break;
-                case RIGHT_KEY:
-                    model.moveGeluleRight();
-                    break;
-                case FLIP_KEY:
-                    model.flipGelule();
-                    break;
-                case DOWN_KEY:
-                    model.dipOrAcceptGelule();
-                    break;
-                case DROP_KEY:
-                    model.dropGelule();
-                    break;
-                case HOLD_KEY:
-                    model.holdGelule();
-                    break;
-            }
+            key.applyOn(model);
+            if (key.canBeHold())
+                pressedKeys.put(key, Timer.ofMilliseconds(75, () -> key.applyOn(model)));
         });
         return true;
     }
 
     @Override
     public boolean keyUp(int keycode) {
+        Key.ofCode(keycode).ifPresent(pressedKeys::remove);
         return true;
     }
 
