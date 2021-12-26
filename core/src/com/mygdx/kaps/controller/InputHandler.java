37a67612de11f231cd97ca8a3d23dcc019c2a556
adam <82466060+osef-art@users.noreply.github.com>
2021-12-26 01:28:42 +0100

    Implement various refresh rate for holdable keys

--- core/src/com/mygdx/kaps/controller/InputHandler.java
+++ core/src/com/mygdx/kaps/controller/InputHandler.java
@@ -13,10 +13,10 @@ import java.util.stream.Collectors;
 
 public class InputHandler implements InputProcessor {
     public enum Key {
-        LEFT_KEY(Level::moveGeluleLeft, true, 21, 45), // Q, LEFT ARR.
-        RIGHT_KEY(Level::moveGeluleRight, true, 22, 32), // D, RIGHT ARR.
-        DOWN_KEY(Level::dipOrAcceptGelule, true, 20, 47), // S, DOWN ARR.
-        FLIP_KEY(Level::flipGelule, true, 19, 54), // Z, UP ARR.
+        LEFT_KEY(Level::moveGeluleLeft, 75.0, 21, 45), // Q, LEFT ARR.
+        RIGHT_KEY(Level::moveGeluleRight, 75.0, 22, 32), // D, RIGHT ARR.
+        DOWN_KEY(Level::dipOrAcceptGelule, 75.0, 20, 47), // S, DOWN ARR.
+        FLIP_KEY(Level::flipGelule, 100.0, 19, 54), // Z, UP ARR.
         DROP_KEY(Level::dropGelule, 62), // C, L
         HOLD_KEY(Level::holdGelule, 31, 50, 30), // V, M
 
@@ -25,20 +25,16 @@ public class InputHandler implements InputProcessor {
 
         private final Set<Integer> codes;
         private final Consumer<Level> effect;
-        private final boolean holdable;
+        private final double refreshRate;
 
         Key(Consumer<Level> consumer, int... codes) {
-            this(consumer, false, codes);
+            this(consumer, 0, codes);
         }
 
-        Key(Consumer<Level> consumer, boolean hold, int... codes) {
+        Key(Consumer<Level> consumer, double hold, int... codes) {
             this.codes = Arrays.stream(codes).boxed().collect(Collectors.toUnmodifiableSet());
             effect = consumer;
-            holdable = hold;
-        }
-
-        public static Optional<Key> ofCode(int code) {
-            return Arrays.stream(values()).filter(k -> k.codes.contains(code)).findFirst();
+            refreshRate = hold;
         }
 
         @Override
@@ -46,12 +42,12 @@ public class InputHandler implements InputProcessor {
             return "[" + super.toString().split("_")[0] + "]";
         }
 
-        public void applyOn(Level level) {
-            effect.accept(level);
+        private static Optional<Key> ofCode(int code) {
+            return Arrays.stream(values()).filter(k -> k.codes.contains(code)).findFirst();
         }
 
-        public boolean canBeHold() {
-            return holdable;
+        private boolean canBeHold() {
+            return refreshRate > 0;
         }
     }
 
@@ -72,9 +68,9 @@ public class InputHandler implements InputProcessor {
         // debug
 //        System.out.println(keycode);
         Key.ofCode(keycode).ifPresent(key -> {
-            key.applyOn(model);
+            key.effect.accept(model);
             if (key.canBeHold())
-                pressedKeys.put(key, Timer.ofMilliseconds(75, () -> key.applyOn(model)));
+                pressedKeys.put(key, Timer.ofMilliseconds(key.refreshRate, () -> key.effect.accept(model)));
         });
         return true;
     }
