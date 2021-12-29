4d04be18e4bc74b60b805df45e366301e93aa388
adam <82466060+osef-art@users.noreply.github.com>
2021-12-29 02:37:41 +0100

    Add Game Launchers

--- core/src/com/mygdx/kaps/MainScreen.java
+++ core/src/com/mygdx/kaps/MainScreen.java
@@ -17,8 +17,21 @@ public class MainScreen extends ApplicationAdapter {
     private InputHandler inputs;
     public static SoundStream soundStream;
 
+    private final String[] args;
     private Level game;
     private GameView view;
+
+    public MainScreen(String... args) {
+        this.args = args;
+    }
+
+    private Level loadedLevel() {
+        if (args.length > 0 && args[0].equals("-l")) {
+            int lvl = args.length > 1 ? Integer.parseInt(args[1]) : new Random().nextInt(21);
+            return LevelLoader.loadFrom("android/assets/levels/level" + lvl);
+        }
+        return LevelLoader.randomLevel(6, 15, 10);
+    }
 
     @Override
     public void create() {
@@ -26,9 +39,7 @@ public class MainScreen extends ApplicationAdapter {
         camera.setToOrtho(true);
         camera.translate(0, Gdx.graphics.getHeight());
 
-        game = new Random().nextBoolean() ?
-                 LevelLoader.loadFrom("android/assets/levels/level" + new Random().nextInt(21)) :
-                 LevelLoader.randomLevel(6, 15, 10);
+        game = loadedLevel();
         view = new GameView(game);
         inputs = new InputHandler(game);
 
--- desktop/src/com/mygdx/kaps/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/kaps/desktop/DesktopLauncher.java
@@ -6,16 +6,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.kaps.MainScreen;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "KAPS";
-		config.width = 480;
-		config.height = 800;
-		config.vSyncEnabled = true;
-		config.pauseWhenMinimized = true;
-		config.pauseWhenBackground = true;
-		config.addIcon("android/assets/sprites/icons/icon.png", Files.FileType.Local);
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "KAPS";
+        config.width = 480;
+        config.height = 800;
+        config.vSyncEnabled = true;
+        config.pauseWhenMinimized = true;
+        config.pauseWhenBackground = true;
+        config.addIcon("android/assets/sprites/icons/icon.png", Files.FileType.Local);
 
-		new LwjglApplication(new MainScreen(), config);
-	}
+        new LwjglApplication(new MainScreen(args), config);
+    }
 }
