f986b1633700cc7ae51426749ae4484ec31e6f8e
adam <82466060+osef-art@users.noreply.github.com>
2022-01-10 19:16:29 +0100

    Optimize code - Remove sidekick supplier

--- core/src/com/mygdx/kaps/level/Sidekick.java
+++ core/src/com/mygdx/kaps/level/Sidekick.java
@@ -1,11 +1,11 @@
 package com.mygdx.kaps.level;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.mygdx.kaps.Utils;
 import com.mygdx.kaps.level.gridobject.Color;
 import com.mygdx.kaps.renderer.AnimatedSprite;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -24,31 +24,36 @@ interface ISidekick {
 }
 
 public abstract class Sidekick implements ISidekick {
-    enum SidekickData {
+    enum SidekickId {
         SEAN(Color.COLOR_1, 20),
         ZYRAME(Color.COLOR_2, 18),
         R3D(Color.COLOR_3, 25, "Red"),
         MIMAPS(Color.COLOR_4, 15),
         PAINTER(Color.COLOR_5, 10, "Paint"),
         XERETH(Color.COLOR_6, 25),
-        BOMBER(Color.COLOR_7, 13),
+        BOMBER(Color.COLOR_7, 13, true),
         JIM(Color.COLOR_10, 18),
-        UNI(Color.COLOR_11, 4, "Color"),
+        UNI(Color.COLOR_11, 4, true, "Color"),
         SNIPER(Color.COLOR_12, 20),
         ;
 
+        private final boolean passive;
         private final String animPath;
         private final Color color;
         private final int mana;
 
-        SidekickData(Color color, int mana, String... names) {
+        SidekickId(Color color, int mana, boolean passive, String... names) {
             var name = names.length > 0 ? names[0] : toString();
             animPath = "android/assets/sprites/sidekicks/" + name + "_";
+            this.passive = passive;
             this.color = color;
             this.mana = mana;
         }
 
-        @Override
+        SidekickId(Color color, int mana, String... names) {
+            this(color, mana, false, names);
+        }
+
         public String toString() {
             var str = super.toString();
             return str.charAt(0) + str.substring(1).toLowerCase();
@@ -58,41 +63,27 @@ public abstract class Sidekick implements ISidekick {
             return mana;
         }
     }
-    enum SidekickSupplier {
-        SEAN(new ManaSidekick(SidekickData.SEAN)),
-        ZYRAME(new ManaSidekick(SidekickData.ZYRAME)),
-        R3D(new ManaSidekick(SidekickData.R3D)),
-        MIMAPS(new ManaSidekick(SidekickData.MIMAPS)),
-        PAINTER(new ManaSidekick(SidekickData.PAINTER)),
-        XERETH(new ManaSidekick(SidekickData.XERETH)),
-        BOMBER(new CooldownSidekick(SidekickData.BOMBER)),
-        JIM(new ManaSidekick(SidekickData.JIM)),
-        UNI(new CooldownSidekick(SidekickData.UNI)),
-        SNIPER(new ManaSidekick(SidekickData.SNIPER)),
-        ;
-
-        private final Sidekick sidekick;
-
-        SidekickSupplier(Sidekick sdk) {
-            sidekick = sdk;
-        }
-    }
 
     private final AnimatedSprite flippedAnim;
     private final AnimatedSprite anim;
     private final Color color;
 
-    Sidekick(SidekickData data) {
-        color = data.color;
-        anim = new AnimatedSprite(data.animPath, 4, 0.2f);
+    Sidekick(SidekickId data) {
         flippedAnim = new AnimatedSprite(data.animPath, 4, 0.2f, true, true);
+        anim = new AnimatedSprite(data.animPath, 4, 0.2f);
+        color = data.color;
     }
 
     static Set<Sidekick> randomSet(int n) {
-        var sidekicks = new HashSet<SidekickSupplier>();
-        while (sidekicks.size() < n)
-            sidekicks.add(Utils.getRandomFrom(Arrays.stream(SidekickSupplier.values())));
-        return sidekicks.stream().map(s -> s.sidekick).collect(Collectors.toSet());
+        var sdkList = Arrays.stream(SidekickId.values())
+          .map(Sidekick::ofId)
+          .collect(Collectors.toList());
+        Collections.shuffle(sdkList);
+        return new HashSet<>(sdkList.subList(0, n));
+    }
+
+    static Sidekick ofId(SidekickId id) {
+        return id.passive ? new CooldownSidekick(id) : new ManaSidekick(id);
     }
 
     public Color color() {
@@ -113,23 +104,22 @@ public abstract class Sidekick implements ISidekick {
     }
 
     void trigger() {
-
     }
 
     void triggerIfReady() {
         if (isReady()) {
             trigger();
-          resetGauge();
+            resetGauge();
         }
     }
 }
 
-class ManaSidekick extends Sidekick{
+class ManaSidekick extends Sidekick {
     private final Gauge mana;
 
-    ManaSidekick(Sidekick.SidekickData data) {
-        super(data);
-        this.mana = new Gauge(data.gaugeMax());
+    ManaSidekick(SidekickId id) {
+        super(id);
+        this.mana = new Gauge(id.gaugeMax());
     }
 
     public String toString() {
@@ -151,19 +141,17 @@ class ManaSidekick extends Sidekick{
         mana.empty();
     }
 
-
-    @Override
     public boolean isReady() {
         return mana.isFull();
     }
 }
 
-class CooldownSidekick extends Sidekick{
+class CooldownSidekick extends Sidekick {
     private final Gauge cooldown;
 
-    CooldownSidekick(Sidekick.SidekickData data) {
-        super(data);
-        this.cooldown = Gauge.full(data.gaugeMax());
+    CooldownSidekick(SidekickId id) {
+        super(id);
+        this.cooldown = Gauge.full(id.gaugeMax());
     }
 
     public String toString() {
@@ -185,7 +173,6 @@ class CooldownSidekick extends Sidekick{
         cooldown.fill();
     }
 
-    @Override
     public boolean isReady() {
         return cooldown.isEmpty();
     }
