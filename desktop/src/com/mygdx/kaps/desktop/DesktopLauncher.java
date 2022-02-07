1739ab83ff53e87b01c0e2e1c965b470577d0655
adam <82466060+osef-art@users.noreply.github.com>
2022-02-07 03:25:51 +0100

    Update build to Lwjgl3

--- desktop/src/com/mygdx/kaps/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/kaps/desktop/DesktopLauncher.java
@@ -1,23 +1,20 @@
 package com.mygdx.kaps.desktop;
 
 import com.badlogic.gdx.Files;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.kaps.MainScreen;
 
 public class DesktopLauncher {
     public static void main(String[] args) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "KAPS";
-        config.width = 480;
-        config.height = 800;
-        config.vSyncEnabled = true;
-        config.resizable = false;
-        config.backgroundFPS = -1;
-        config.pauseWhenMinimized = true;
-        config.pauseWhenBackground = true;
-        config.addIcon("android/assets/sprites/icons/icon.png", Files.FileType.Local);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("KAPS");
+        config.setWindowIcon(Files.FileType.Local, "android/assets/sprites/icons/icon.png");
 
-        new LwjglApplication(new MainScreen(args), config);
+        config.setWindowedMode(480, 800);
+        config.setResizable(false);
+        config.useVsync(true);
+
+        new Lwjgl3Application(new MainScreen(args), config);
     }
 }
